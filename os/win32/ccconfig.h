22e2476258ef2bc8a51b22e33e2aef6ea329871a
Antoine Leca <antoine64leca@hotmail.com>
2013-02-18 16:01:23 +0100

    Fix working on WIN32
    Update the MingW32 and Microsoft CL ports to match the new
    framework for the driver.
    
    Also revert most of the changes done from #ifdef _WIN32 to #ifdef os_win32:
    the protected code is meant to deal with peculiarities when running on
    Windows, not with peculiarities when targeting Windows.
    
    Work is still need to restore the profiling of code: with MingW32
    the (GPL'ed) infrastructure uses crt2.o gcrt2.o (2 files), while
    on *nix only one file is used (gcrt0.o or gcrt1.o)
    
    Building DLL (-shared, dllcrt2.o) has not been tested. It will need
    some hook in running_linker(), such as PCC_{EARLY,LATE}_LD_ARGS

--- os/win32/ccconfig.h
+++ os/win32/ccconfig.h
@@ -9,13 +9,33 @@
 #define	CPPADD { "-DWIN32", NULL }
 #define LIBCLIBS { "/subsystem:console", "msvcrt.lib", "libpcc.a", NULL }
 #else
-/* requires w32api-3.2.tar.gz and mingw-runtime-3.14.tar.gz */
-#define	CPPADD { "-DWIN32", "-D__MSVCRT__", "-D__MINGW32__", NULL }
 #define STARTFILES { LIBDIR "crt2.o", NULL }
 #define ENDFILES { NULL }
 #define STARTFILES_S { LIBDIR "dllcrt2.o", NULL }
 #define ENDFILES_S { NULL }
 #define LIBCLIBS { "-lmoldname", "-lmingwex", "-lmingw32", "-lpcc", "-lmsvcrt", "-luser32", "-lkernel32", NULL }
+
+/* common cpp predefines */
+#define	CPPADD { "-DWIN32", "-D_WIN32", "-D__MSVCRT__", "-D__MINGW32__", NULL }
+
+/* host-dependent */
+/* requires w32api-3.2.tar.gz and mingw-runtime-3.14.tar.gz */
+#define CRT0		"crt2.o"
+#define CRT0_S		"dllcrt2.o"	/* not foreseen so far */
+#define GCRT0		"gcrt2.o"	/* in _addition_ to either crt2.o or dllcrt2.o */
+#define	CRTBEGIN_S	"crtbegin.o"
+#define	CRTEND_S	"crtend.o"
+#define CRTI		0
+#define CRTN		0
+
+#define DEFLIBDIRS	{ LIBDIR, 0 }	/* overrides "/usr/lib" */
+#define WIN32_LIBC	"-lmsvcrt"
+#define MINGW_RTLIBS	"-lmoldname", "-lmingwex", "-lmingw32"
+#define MINGW_SYSLIBS	"-luser32", "-lkernel32" /* ,"-ladvapi32", "-lshell32" */
+#define	DEFLIBS		{ MINGW_RTLIBS, "-lpcc", WIN32_LIBC, MINGW_SYSLIBS, MINGW_RTLIBS, WIN32_LIBC, 0 }
+#define	DEFPROFLIBS	{ MINGW_RTLIBS, "-lpcc", WIN32_LIBC, "-lgmon", MINGW_SYSLIBS, MINGW_RTLIBS, WIN32_LIBC, 0 }
+#define	DEFCXXLIBS	{ "-lp++", MINGW_RTLIBS, "-lpcc", WIN32_LIBC, MINGW_SYSLIBS, MINGW_RTLIBS, WIN32_LIBC, 0 }
+
 #endif
 
 #define CPPMDADD { "-D__i386__", NULL }
